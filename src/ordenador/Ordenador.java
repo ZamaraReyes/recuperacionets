9771e6df8b808d2d8c306544d1c165ed8dbd4c6b
Zare Red <ZareRed@MacBook-Pro-de-Laura.local>
2016-04-19 19:15:30 +0100

    paso 1 y 2

---
+++ src/ordenador/Ordenador.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ordenador;
+
+/**
+ *
+ * @author ZareRed
+ */
+public class Ordenador {
+
+    private String procesador;
+    private String memoria;
+    private String resolucionPantalla;
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
